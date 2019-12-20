810bb26468e2e67b59e29f8d7bd7bfcab40e0bb4
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2019-12-20 09:01:02 -0800

    Project setup. Routes setup. axios API call working.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header.js";
+import axios from "axios";
+import { Route } from 'react-router-dom';
+
+import WelcomePage from './components/WelcomePage'; 
+import CharacterList from './components/CharacterList';
 
 
 export default function App() {
+
+  useEffect(() => {
+
+  },[]);
+
+
+
   return (
     <main>
       <Header />
+      
+      <Route exact path="/" component={WelcomePage} />
+      <Route path='/character-list' component={CharacterList} />
     </main>
   );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,42 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+
+import CharacterCard from './CharacterCard';
+import { Button } from 'reactstrap';
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [apiData, setApiData] = useState([]);
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get('https://rickandmortyapi.com/api/character/')
+    .then(response => {
+      console.log(response)
+      setApiData(response.data.results);
+    })
+    .catch(err => {
+      console.log(err);
+    })
   }, []);
 
   return (
     <section className="character-list">
       <h2>TODO: `array.map()` over your state here!</h2>
+
+      {apiData.map(character => {
+        return (
+          <div>
+            <h1>{character.name}</h1>
+              <Button>React Button</Button>
+
+          </div>
+
+          )
+      })}
+
     </section>
   );
 }
